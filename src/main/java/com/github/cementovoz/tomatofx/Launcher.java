819846fe9b5c80d7feb3069ee0b02ecbb1da7e21
pelenthium <pelenthium@gmail.com>
2015-01-14 08:49:34 +0300

    Added gitignore, set transparent style for window, added background image

--- src/main/java/com/github/cementovoz/tomatofx/Launcher.java
+++ src/main/java/com/github/cementovoz/tomatofx/Launcher.java
@@ -3,6 +3,7 @@ package com.github.cementovoz.tomatofx;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * Created by cementovoz on 13.01.15.
@@ -12,9 +13,11 @@ public class Launcher extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         TimerController timer = new TimerController();
-        Scene scene = new Scene(timer, 400, 400);
+        Scene scene = new Scene(timer, 350, 420);
         scene.getStylesheets().add(Launcher.class.getResource("/com/github/cemenetovoz/tomatofx/css/styles.css").toExternalForm());
+        scene.setFill(null);
         primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
     }
 
--- src/main/java/com/github/cementovoz/tomatofx/TimerController.java
+++ src/main/java/com/github/cementovoz/tomatofx/TimerController.java
@@ -1,13 +1,47 @@
 package com.github.cementovoz.tomatofx;
 
+import javafx.beans.binding.Binding;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.util.converter.DateTimeStringConverter;
+import javafx.util.converter.NumberStringConverter;
+
 
 /**
  * Created by cementovoz on 13.01.15.
  */
 public class TimerController extends AnchorPane {
 
+
+    private final Label seconds = new Label();
+    private final Label minutes = new Label();
+    private final Label separator = new Label();
+
+    private SimpleObjectProperty<Date>
+
     public TimerController () {
+        getStyleClass().add("timer");
+        Bindings.bindBidirectional(minutes.textProperty(),
+                minuteProperty,
+                new DateTimeStringConverter());
+
+        HBox hBox = new HBox() {{
+            getStyleClass().add("container-text");
+            getChildren().addAll(minutes, separator, seconds);
+        }};
+        AnchorPane.setTopAnchor(hBox, 170d);
+        AnchorPane.setLeftAnchor(hBox, 0d);
+        AnchorPane.setRightAnchor(hBox, 0d);
+        hBox.setAlignment(Pos.BASELINE_CENTER);
+        getChildren().addAll(hBox);
+
 
     }
 
